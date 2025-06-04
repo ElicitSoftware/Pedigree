0c6f109c21bde792f079a01150ec79a607d8ec22
Matthew Demerath <m.demerath@elicitsoftware.com>
2025-06-04 08:17:43 -0400

    Added legend

--- plumber.R
+++ plumber.R
@@ -21,7 +21,7 @@ function(ped) {
   require(quadprog)
   require(kinship2)
 
-    # Access the file content from the list ped[[1]]
+  # Access the file content from the list ped[[1]]
   file_content <- ped[[1]]
 
   data_df <- read.table(text = file_content, header = TRUE,  sep = "\t", na.strings = "NA")
@@ -30,10 +30,21 @@ function(ped) {
   ids <- paste(data_df$ID, gsub("_", " ",gsub("-", "\n", data_df$Label)), sep="\n")
   cols <- c(ifelse(data_df$ID == 7, 3,ifelse(data_df$ul + data_df$ur + data_df$ll + data_df$lr > 0  ,2 ,1)))
   myped <- pedigree(id=data_df$ID, dadid=data_df$Dadid, momid=data_df$Momid, sex=data_df$Sex, status=data_df$Status, affected=as.matrix(aff), famid=data_df$Ped)
-  plot(myped['1'], ids, mar=c(2, 2, 2, 2), symbolsize = 1.2, col=cols, cex=.7)
 
+  # Increase margins if necessary - make bottom margin larger for text
+  par(mar = c(6, 4, 4, 4))  # Increased bottom margin from 4 to 6
+
+  plot(myped['1'], ids, symbolsize = 1.2, col=cols, cex=.7)
+
+  # Add text at bottom of plot - two lines with different colors
+  mtext("Green = Respondent", side = 1, line = 3, cex = 0.8, col = "green")
+  mtext("Red = Family member with cancer", side = 1, line = 4, cex = 0.8, col = "red")
+
+  # Check if '*' exists in the file content and add line if found
+  if (grepl("\\*", file_content)) {
+    mtext("* Indicates multiple diagnoses of the same cancer type.", side = 1, line = 5, cex = 0.8, col = "black")
+  }
 }
-
 
 #* Generates a pedigree svg image from a pedigree data frame
 #* @post /pdf
@@ -42,7 +53,6 @@ function(ped) {
 function(ped) {
 
     # Load necessary libraries
-
     require(plumber)
     require(Matrix)
     require(quadprog)
@@ -60,16 +70,20 @@ function(ped) {
     cols <- c(ifelse(data_df$ID == 7, 3,ifelse(data_df$ul + data_df$ur + data_df$ll + data_df$lr > 0  ,2 ,1)))
     myped <- pedigree(id=data_df$ID, dadid=data_df$Dadid, momid=data_df$Momid, sex=data_df$Sex, status=data_df$Status, affected=as.matrix(aff), famid=data_df$Ped)
 
-
     # Increase margins if necessary
-    par(mar = c(4, 4, 4, 4))  # Adjust the margin size
+    par(mar = c(6, 4, 4, 4))  # Increased bottom margin to match SVG
 
     # Plot the pedigree with the updated color scheme
     plot(myped['1'], ids, symbolsize = 1.2, col = cols, cex = .7)
 
-    # Add the legend
-    legend(x = 0.8, y = 0.2, legend = c("green = Respondent", "red = Family member with cancer"),
-        text.col = c("darkgreen", "red"), bty = "n", cex = 1.0, inset = c(-0.1, -0.1))
+    # Add text at bottom of plot - two lines with different colors
+    mtext("Green = Respondent", side = 1, line = 3, cex = 0.8, col = "green")
+    mtext("Red = Family member with cancer", side = 1, line = 4, cex = 0.8, col = "red")
+
+    # Check if '*' exists in the file content and add line if found
+    if (grepl("\\*", file_content)) {
+      mtext("* Indicates multiple diagnoses of the same cancer type.", side = 1, line = 5, cex = 0.8, col = "black")
+    }
 
     # Close the PDF device
     dev.off()
